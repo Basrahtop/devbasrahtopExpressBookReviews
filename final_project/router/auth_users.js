253b3013f913f128bb63f3baa1eb7f5ae7b31650
Abdulrahman Abdulbasit Adigun <85456426+Basrahtop@users.noreply.github.com>
2023-06-04 01:41:45 -0700

    Update auth_users.js
    I changed the regd_users to registered_users for easy readability and understanding the code

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -14,17 +14,17 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
+registered_users.post("/login", (req,res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
+registered_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-module.exports.authenticated = regd_users;
+module.exports.authenticated = registered_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
