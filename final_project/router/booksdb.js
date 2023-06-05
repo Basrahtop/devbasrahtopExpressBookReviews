57725461d0650c8dbb8c23bdadb4a8829a60ac89
Abdulrahman Abdulbasit Adigun <85456426+Basrahtop@users.noreply.github.com>
2023-06-05 04:27:53 -0700

    Update booksdb.js
    Included isbn and it value as part of the books object requirement

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Baltazar Bolado","title": "Publius Sec Semper Tyrannis", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Baltazar Bolado","title": "Common Sense", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Baltazar Bolado","title": "Publius Sec Semper Tyrannis", "reviews": {}, "isbn": "1665905581" },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {}, "isbn": "B093TM26P4" },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {}, "isbn": "B09ZN4WVQJ" },
+      4: {"author": "Baltazar Bolado","title": "Common Sense", "reviews": {}, "isbn": "B09L57P3XL" },
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {}, "isbn": "1538742578" },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {}, "isbn": "B09S3ZBVN9" },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {}, "isbn": "0062879928" },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {}, "isbn": "B09Z3D5B34" },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {}, "isbn": "0063204207" },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {}, "isbn": "1982177748" }
 }
 
 module.exports=books;
